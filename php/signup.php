9ad100b646740ebfa9cf91a2cd5bf90754a49da6
iihakiroii <cesar.malave.lara@gmail.com>
2024-02-07 22:10:12 -0400

    fix: signup was fixed for the error br

--- php/signup.php
+++ php/signup.php
@@ -6,36 +6,30 @@
   $password = mysqli_real_escape_string($conn, $_POST['password']);
 
   if(!empty($fname) && !empty($lname) && !empty($email) && !empty($password)){
-    //checking user email is valid or not
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)){ //if email is valid
-      // checking email already exist in the database or not
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
       $sql = mysqli_query($conn, "SELECT email FROM users WHERE email = '{$email}'");
-      if(mysqli_num_rows($sql) > 0){ //if email alreadt exist
-        echo "$email - This email already exist!"
+      if(mysqli_num_rows($sql) > 0){
+        echo "$email - This email already exist!";
       }else{
-        //checking user upload file or not
-        if(isset($_FILES['image'])){ //if file is uploaded
-          $img_name = $_FILES['image']['name']; //getting user uploaded img name
-          $img_name = $_FILES['image']['tmp_name']; //this temporary name is used to save file in the folder
+        if(isset($_FILES['image'])){
+          $img_name = $_FILES['image']['name'];
+          $tmp_name = $_FILES['image']['tmp_name'];
 
-          //exploding image and get the last extension like jpg png
-          $img_explode = explode('.', $img_name);
-          $img_ext = end($img_explode); //getting the extension on an user uploaded img file
+          $img_explode =  explode('.', $img_name);
+          $img_ext = end($img_explode);
 
           $extensions = ['png', 'jpeg', 'jpg'];
-          if(in_array($img_ext, $extensions) === true){ 
+          if(in_array($img_ext, $img_ext) === true){
             $time = time();
-
             $new_img_name = $time.$img_name;
-
+            
             if(move_uploaded_file($tmp_name, "images/".$new_img_name)){
               $status = "Active now";
               $random_id = rand(time(), 10000000);
 
-              //inserting all user data inside table
               $sql2 = mysqli_query($conn, "INSERT INTO users (unique_id, fname, lname, email, password, img, status)
-                        VALUES ({$random_id}, '{$fname}', '{$lname}', '{$email}', '{$password}', '{$new_img_name}', '{$status}')");
-              if($sql2){
+                      VALUES ({$random_id}, '{$fname}', '{$lname}', '{$email}', '{$password}', '{$new_img_name}', '{$status}')");
+              if(sql2){
                 $sql3 = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'");
                 if(mysqli_num_rows($sql3) > 0){
                   $row = mysqli_fetch_assoc($sql3);
@@ -49,15 +43,15 @@
 
           }else{
             echo "Please select an Image file - jpeg, jpg, png!";
-          } 
+          }
+
         }else{
           echo "Please select an Image file!";
         }
       }
     }else{
-      echo "$email - This is not a valid email!";
+      echo "$email - This is not a valid email";
     }
-
   }else{
     echo "All input field are required";
   }
