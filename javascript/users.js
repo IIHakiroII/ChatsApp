f5706899798fb0746e3c7809265cee6692f52666
rafaelb056 <rafaelbenitez056@gmail.com>
2024-02-19 09:21:50 -0400

    fix: data.php added with id :D

--- javascript/users.js
+++ javascript/users.js
@@ -6,17 +6,23 @@ searchBtn.onclick = ()=>{
   searchBar.classList.toggle("active");
   searchBar.focus();
   searchBtn.classList.toggle("active");
+  searchBar.value = "";
 }
 
 searchBar.onkeyup = ()=>{
   let searchTerm = searchBar.value;
+  if(searchTerm != ""){
+    searchBar.classList.add("active");
+  }else{
+    searchBar.classList.remove("active");
+  }
   let xhr = new XMLHttpRequest(); //creating XML object
   xhr.open("POST", "php/search.php", true);
   xhr.onload = ()=>{
     if(xhr.readyState === XMLHttpRequest.DONE){
       if(xhr.status === 200){
         let data = xhr.response;
-        console.log(data);
+        usersList.innerHTML = data;
       }
     }
   }
@@ -31,7 +37,9 @@ setInterval(()=>{
     if(xhr.readyState === XMLHttpRequest.DONE){
       if(xhr.status === 200){
         let data = xhr.response;
-        usersList.innerHTML = data;
+        if(!searchBar.classList.contains("active")){
+          usersList.innerHTML = data;
+        }
       }
     }
   }
