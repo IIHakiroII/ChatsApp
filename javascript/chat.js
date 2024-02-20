1bdbe3f769bd57b68f814bcd6aae98c068b7ac54
iihakiroii <cesar.malave.lara@gmail.com>
2024-02-20 14:55:45 -0400

    fix: scroll finished in chat.js

--- javascript/chat.js
+++ javascript/chat.js
@@ -15,12 +15,20 @@ sendBtn.onclick = ()=>{
     if(xhr.readyState === XMLHttpRequest.DONE){
       if(xhr.status === 200){
         inputField.value = "";
+        scrollToBottom();
       }
     }
   }
   // we have to send the form data through ajax to php
   let formData = new FormData(form); //creating new formData object
   xhr.send(formData); //sending the form data to php
+}
+
+chatBox.onmouseenter = ()=>{
+  chatBox.classList.add("active");
+}
+chatBox.onmouseleave = ()=>{
+  chatBox.classList.remove("active");
 }
 
 setInterval(()=>{
@@ -32,6 +40,9 @@ setInterval(()=>{
       if(xhr.status === 200){
         let data = xhr.response;
         chatBox.innerHTML = data;
+        if(!chatBox.classList.contains("active")){ //if active class not contains in chatbox the scroll to bottom
+          scrollToBottom();
+        }
       }
     }
   }
@@ -40,3 +51,7 @@ setInterval(()=>{
   let formData = new FormData(form); //creating new formData object
   xhr.send(formData); //sending the form data to php
 }, 500); //this function run frequently after 500ms
+
+function scrollToBottom(){
+  chatBox.scrollTop = chatBox.scrollHeight;
+}
