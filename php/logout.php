a0baba49aa7f40592d25f56147e2bab06f7e7734
rafaelb056 <rafaelbenitez056@gmail.com>
2024-02-21 08:31:46 -0400

    fix: project finished

--- php/logout.php
+++ php/logout.php
@@ -1,7 +1,7 @@
 <?php 
   session_start();
   if(isset($_SESSION['unique_id'])){ //if user is logged in then come to this page otherwise go to login page
-    include_once "config/php";
+    include_once "config.php";
     $logout_id = mysqli_real_escape_string($conn, $_GET['logout_id']);
     if(isset($logout_id)){//if logout id is set
       $status = "Offline now";
